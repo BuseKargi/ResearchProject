2d4823faea6a12abd22ed3af820f1c45160a37d4
Buse Kargı <busekargi6199@gmail.com>
2024-01-02 19:50:18 +0300

    login methods added login controller

--- Budget Project/Budget Project/Controllers/LoginController.cs
+++ Budget Project/Budget Project/Controllers/LoginController.cs
@@ -1,17 +1,34 @@
-﻿using System;
+﻿using Budget_Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Budget_Project.Controllers
+namespace MyBudget.Controllers
 {
     public class LoginController : Controller
     {
+        MyBudgetEntities db = new MyBudgetEntities();
         // GET: Login
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User model)
+        {
+            User data = db.User.Where(x => x.Email == model.Email && x.Password == model.Password).SingleOrDefault();
+            if (data != null)
+            {
+                CurrentSession.Set<User>("login", data);
+                return RedirectToAction("/", "Home");
+
+            }
+            return View(model);
+
+
         }
     }
 }
--- Budget Project/Budget Project/Models/CurrentSession.cs
+++ Budget Project/Budget Project/Models/CurrentSession.cs
@@ -1,5 +1,5 @@
 ﻿
-using MyBudget.Models;
+using Budget_Project.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
