9477f00ec0f85a8f3b44d00b28de21e93236a5e8
Buse Kargı <busekargi6199@gmail.com>
2024-01-03 13:43:50 +0300

    create method added in controller and added create view

--- Budget Project/Budget Project/Controllers/AccountController.cs
+++ Budget Project/Budget Project/Controllers/AccountController.cs
@@ -22,5 +22,32 @@ namespace Budget_Project.Controllers
             var account = db.Account.Where(x => x.UserId == currentUser).ToList();
             return View(account);
         }
+        [Auth]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Account model, HttpPostedFileBase LogoPath)
+        {
+            if (LogoPath != null)
+            {
+                string _LogoPath = $"{model.Name}.{LogoPath.ContentType.Split('/')[1]}";
+                LogoPath.SaveAs(Server.MapPath($"~/Assets/images/account/{_LogoPath}"));
+                model.Logo = _LogoPath;
+            }
+            else
+            {
+                model.Logo = null;
+            }
+            var currentUser = CurrentSession.User.Id;
+
+            model.CreatedDate = DateTime.Now;
+            model.ModifiedDate = DateTime.Now;
+            model.UserId = currentUser;
+            db.Account.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
