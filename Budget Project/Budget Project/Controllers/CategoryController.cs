e892a1c898626f9c0da8a9ea63193c10860b80b5
Buse Kargı <busekargi6199@gmail.com>
2024-01-04 12:25:55 +0300

    Category controller and view page added

---
+++ Budget Project/Budget Project/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using Budget_Project.Filters;
+using System.Web.Mvc;
+using Budget_Project.Models;
+
+namespace MyBudget.Controllers
+{
+    public class CategoryController : Controller
+    {
+        MyBudgetEntities db = new MyBudgetEntities();
+        // GET: Category
+        [Auth]
+        public ActionResult Index()
+        {
+            var currentUser = CurrentSession.User.Id;
+            var category = db.Category.Where(x => x.UserId == currentUser).ToList();
+            return View(category);
+        }
+    }
+}
