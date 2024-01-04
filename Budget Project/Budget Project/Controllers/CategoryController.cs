11b39e7926d18ee2f9f63f7931809d45bd42023f
Buse Kargı <busekargi6199@gmail.com>
2024-01-04 12:28:45 +0300

    category create method and view page added

--- Budget Project/Budget Project/Controllers/CategoryController.cs
+++ Budget Project/Budget Project/Controllers/CategoryController.cs
@@ -22,5 +22,42 @@ namespace MyBudget.Controllers
             var category = db.Category.Where(x => x.UserId == currentUser).ToList();
             return View(category);
         }
+        [Auth]
+        public ActionResult Create()
+        {
+            ViewBag.ParentId = new SelectList(db.Category.Where(x => x.ParentId == 0 && x.IsActive == true).ToList(), "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category model, HttpPostedFileBase LogoPath)
+        {
+
+            if (ModelState.IsValid)
+            {
+                ViewBag.ParentId = new SelectList(db.Category.Where(x => x.ParentId == 0 && x.IsActive == true).ToList(), "Id", "Name");
+
+                if (LogoPath != null)
+                {
+                    string _LogoPath = $"{model.Name}.{LogoPath.ContentType.Split('/')[1]}";
+                    LogoPath.SaveAs(Server.MapPath($"~/Assets/images/category/{_LogoPath}"));
+                    model.Logo = _LogoPath;
+                }
+                else
+                {
+                    model.Logo = null;
+                }
+
+                var currentUser = CurrentSession.User.Id;
+
+                model.ParentId = model.ParentId == null ? 0 : model.ParentId;
+                model.UserId = currentUser;
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                db.Category.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
     }
 }
